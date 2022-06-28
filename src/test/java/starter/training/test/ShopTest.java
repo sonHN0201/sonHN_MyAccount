535b9bf0b5e09cf2aa0b0764d64c491906a8565f
sonHN0201 <sonhn85021@co-well.vn>
2022-06-28 18:09:03 +0700

    28/6/2022

--- src/test/java/starter/training/test/ShopTest.java
+++ src/test/java/starter/training/test/ShopTest.java
@@ -1,4 +1,75 @@
 package starter.training.test;
 
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.annotations.CastMember;
+import net.thucydides.core.annotations.Title;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SerenityJUnit5Extension.class)
 public class ShopTest {
+    @CastMember(name = "sonHN")
+    Actor sonHN;
+
+    @Test
+    @Title("Verify that the user can only view books for 150 to 450 rps")
+    public static void tc_01(){
+
+    }
+    @Test
+    @Title("Verify that the user can only view that specific product")
+    public static void tc_02(){
+
+    }
+    @Test
+    @Title("Verify that users can only view popular products")
+    public static void tc_03(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_04(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_05(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_06(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_07(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_08(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_09(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_10(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_11(){
+
+    }
+    @Test
+    @Title("")
+    public static void tc_12(){
+
+    }
 }
